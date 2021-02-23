9bfd294e2697578d604554ac1e441ad69e9d0a59
schaffjc <schaffjc@mail.uc.edu>
2021-02-23 15:38:31 +0000

    updates

--- Main.java
+++ Main.java
@@ -2,17 +2,19 @@ class Main {
   public static void main(String[] args) {
     Student student1 = new Student();
 
-    student1.setName("Jamie Schaffer");
+    student1.setName("Arnold Spielberg");
     student1.setGPA(4.00);
-    student1.setMajor("Accounting");
+    student1.setMajor("Electrical Engineering");
 
-    student1.name = "Jamie Schaffer";
+/* do this with dot operator
+    student1.name = "Arnold Spielberg";
     student1.gpa = 4.0;
-    student1.major = "Accounting";
+    student1.major = "Electrical Engineering";
+*/
 
     System.out.println("student1's name is " + student1.getName() + " and their GPA is " + student1.getGPA() + " and their major is " + student1.getMajor());
 
-    System.out.println("student1's name is " + student1.name + " and their GPA is " + student1.gpa + " and their major is " + student1.major); 
-     
+   /* System.out.println("student1's name is " + student1.name + " and their GPA is " + student1.gpa + " and their major is " + student1.major); 
+   */  
   }
 }
